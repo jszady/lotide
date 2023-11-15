693cdc55f8d516daa9b23ac59d5af11c65e6f552
jszady <jszady99@gmail.com>
2023-11-15 15:50:56 -0500

    countLetters now doesnt go through spaces

--- countLetters.js
+++ countLetters.js
@@ -12,22 +12,23 @@ const countLetters = function (string)
 
   for (const letter of string)
   {
-    if(!result[letter])
-    {
-      result[letter] = 1;
-    }
-    else 
-    {
-      result[letter] += 1;
-    }
-
+    if(letter !== " "){
+      if(!result[letter])
+      {
+        result[letter] = 1;
+      }
+      else 
+      {
+        result[letter] += 1;
+      }
   }
+}
   return result;
 };
 
 console.log(countLetters("LHL"))
 console.log(countLetters(""))
-console.log(countLetters("hello"))
+console.log(countLetters("hello jan"))
 const result = countLetters('LHL');
 assertEqual(result['L'], 2);
 assertEqual(result['H'], 1);
