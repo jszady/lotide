50208b4a579a8baaadaea0d8fa5eecdef1741616
jszady <jszady99@gmail.com>
2023-11-15 19:33:02 -0500

    updated assertObjectEquals.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -47,15 +47,16 @@ const eqObjects = function (object1, object2)
   return true;
 };
 
-const assertObjectEqual = function (actual, expected)
+const assertObjectsEqual = function (actual, expected)
 {
+  const inspect = require('util').inspect; 
   if(eqObjects(actual,expected))
   {
-    console.log(`✅✅✅Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
     return;
     
   }
-  console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+  console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 }
 
-assertObjectEqual({name: "jan", age:22}, {age: 22, name: "jan"});
+assertObjectsEqual({name: "jan", age:22}, {age: 22, name: "jan"});
