6aa689574019021c9b15ea3a83012124c907950c
jszady <jszady99@gmail.com>
2023-11-20 14:50:55 -0500

    updated head file

--- head.js
+++ head.js
@@ -10,13 +10,4 @@ const head = function(arr) {
 
 }
 
-console.log(head([1,2,3,4]));
-console.log(head([]));
-console.log(head([12]));
-console.log(head(['hello','how','are','you']));
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), 0)
-assertEqual(head([,6,7]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,4 @@
 const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
-
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([0]), 0)
