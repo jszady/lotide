e96306c590df23a714619246a5189cdf7411cece
jszady <jszady99@gmail.com>
2023-11-20 15:25:38 -0500

    middle test

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const middle = require('../middle');
+
+
+assertArraysEqual(middle([1,2,3]), [2]);
+assertArraysEqual(middle([1,2,3]), []);
+assertArraysEqual(middle([1,2]), []);
+assertArraysEqual(middle([]), []);
