3f85769b538ea571321ab9a7a954b9b9f87d0928
jszady <jszady99@gmail.com>
2023-11-20 14:52:58 -0500

    updated

--- head.js
+++ head.js
@@ -1,13 +1,8 @@
 const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
-  if(arr[0] === '') {
-    return undefined;
-  }
-
-  let firstElement = arr[0];
-  return firstElement;
+  
+  return arr[0];
 
 }
-
 module.exports = head;
