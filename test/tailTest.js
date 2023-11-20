ff6bb7fc853507db8cdf4c8ac79f16a2b0fa6983
jszady <jszady99@gmail.com>
2023-11-20 15:12:34 -0500

    tail refactor

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,10 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 
 assertEqual(tail(words).length, words.length - 1);
 assertEqual(words.length, 3);
+assertArraysEqual(tail(['hello']), []);
+assertArraysEqual(tail([]),[]);
