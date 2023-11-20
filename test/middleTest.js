5409f583367fd01ca5b721a976f7f56141051669
jszady <jszady99@gmail.com>
2023-11-20 18:16:32 -0500

    mocha and chai test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,26 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([]), []);
+
+// assertArraysEqual(middle([1,2,3]), [2]);
+// assertArraysEqual(middle([1,2,3]), []);
+// assertArraysEqual(middle([1,2]), []);
+// assertArraysEqual(middle([]), []);
+
+describe("middle()", () => {
+  it("should retrun [2] from [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+  it("should retrun [2,3] from [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+  it("should retrun [] from [1,2]", () => {
+    assert.deepEqual(middle([1,2]),[]);
+  });
+  it("should retrun [] from []", () => {
+    assert.deepEqual(middle([]),[]);
+  });
+  
+
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,18 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+const assert = require('chai').assert;
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-assertEqual(tail(words).length, words.length - 1);
-assertEqual(words.length, 3);
-assertArraysEqual(tail(['hello']), []);
-assertArraysEqual(tail([]),[]);
+// assertEqual(tail(words).length, words.length - 1);
+// assertArraysEqual(tail([]),[]);
+describe("tail()", () => {
+  it("return [] if only 1 element ['hello']",() => {
+    assert.deepEqual(tail(['hello']), []);
+  });
+  it("return [] if no element []",() => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("return ['am', 'john'] everything but the first element ['hello','am', 'john'],", () => {
+    assert.deepEqual(tail(['hello','am','john']), ['am', 'john']);
+  });
+})
