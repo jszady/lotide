9aba008e72c22b4577747084ef037191cc797ec5
jszady <jszady99@gmail.com>
2023-11-20 15:47:33 -0500

    added mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,23 @@
+const assert = require('chai').assert;
 const head = require('../head.js');
 const assertEqual = require('../assertEqual.js');
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([0]), 0)
-assertEqual(head([,6,7]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head([0]), 0)
+// assertEqual(head([,6,7]), undefined);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("head()", () => {
+  it('this should return 5 from [5,6,7]', () => {
+    assert.strictEqual(assertEqual(head([5,6,7]), 5));
+  });
+  it('return 0 for [0]', () => {
+    assert.strictEqual(assertEqual(head([0]), 0));
+  });
+  it('return undefined for [,6,7]', () => {
+    assertEqual(head([,6,7]), undefined);
+  });
+  it('this should return hello from ["Hello", "Lighthouse", "Labs"]', () => {
+    assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+})
